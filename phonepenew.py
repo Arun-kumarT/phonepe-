e8b4b05d8c6fb309bb4a2c62daebd9b87765d9f1
Arun-kumarT <165132116+Arun-kumarT@users.noreply.github.com>
2024-04-24 11:11:37 +0530

    Add files via upload

--- phonepenew.py
+++ phonepenew.py
@@ -25,7 +25,7 @@ st.title(":rainbow[PHONEPE DATA VISUALIZATION AND EXPLORATION]",)
 st.write('Note : Data available from 2018 to 2023')
 
 
-tab1,tab2,tab3 = st.tabs(['Transaction','User','Statewise Visualization'])
+tab1,tab2,tab3,tab4= st.tabs(['Transaction','User','Statewise Visualization',"Analysis"])
 with tab1:
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -70,6 +70,7 @@ with tab1:
             agg_count_df,
             geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
             featureidkey='properties.ST_NM',
+            title='Live Geo Visualization of Transactions happened in India',
             locations='State',
             color='Count',
             color_continuous_scale='speed'
@@ -92,6 +93,7 @@ with tab1:
         geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
         featureidkey='properties.ST_NM',
         locations='State',
+        title='Live Geo Visualization of Transactions happened in India',
         color='Amount',
         color_continuous_scale='speed'
         )
@@ -133,10 +135,11 @@ with tab2:
     top_user_df = top_user_df.astype({'User Count':'int64'})
 
     fig = px.choropleth(
-    us_year_df,
+    user_year_df,
     geojson="https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson",
     featureidkey='properties.ST_NM',
     locations='State',
+    title='Live Geo Visualization of Users in India',
     color='Users',
     color_continuous_scale='speed'
     )
@@ -204,4 +207,126 @@ with tab3:
 
     st.write(':blue[District wise Users]')
     st.dataframe(map_user_df,hide_index=True,use_container_width=True)
+with tab4:
+    
+    Querys=st.selectbox("Select the Question",('Top Brands Of Mobiles Used',
+                                               'States With Lowest Trasaction Amount',
+                                               'Districts With Highest Transaction Amount',
+                                               'Top 10 Districts With Lowest Transaction Amount',
+                                               'Top states with highest Users',
+                                               'States With Lowest Transaction Count',
+                                               'States With Highest Transaction Count',
+                                               'States With Highest Transaction Amount',
+                                               'Top 50 Districts With Lowest Transaction Amount'))
+    if Querys=="Top Brands Of Mobiles Used":
+        cursor.execute("select Brand,Users_count from agg_user")
+        user_ag = cursor.fetchall()
+        agg_user=pd.DataFrame(user_ag,columns=["Brand","Users_count"])
+        brand= agg_user[["Brand","Users_count"]]
+        brand1= brand.groupby("Brand")["Users_count"].sum().sort_values(ascending=False)
+        brand2= pd.DataFrame(brand1).reset_index()
+
+        fig_brand= px.pie(brand2, values= "Users_count", names= "Brand", color_discrete_sequence=px.colors.sequential.dense_r,
+                            title= "Top Mobile Brands of Users")
+        st.plotly_chart(fig_brand)
+
+    if Querys=='States With Lowest Trasaction Amount':
+        cursor.execute("select State,Transaction_amount from agg_trans")
+        agg_trans = cursor.fetchall()
+        Aggre_transaction=pd.DataFrame(agg_trans,columns=["States", "Transaction_amount"])
+        at= Aggre_transaction[["States", "Transaction_amount"]]
+        at1= at.groupby("States")["Transaction_amount"].sum().sort_values(ascending= True)
+        at2= pd.DataFrame(at1).reset_index().head(10)
+
+        fig_ats= px.bar(at2, x= "States", y= "Transaction_amount",title= "LOWEST TRANSACTION AMOUNT of States & UT",
+                        color_discrete_sequence= px.colors.sequential.Oranges_r)
+        st.plotly_chart(fig_ats)
+    
+    if Querys=='Districts With Highest Transaction Amount':
+        cursor.execute("select District,Transaction_Amount from map_trans")
+        map_trans = cursor.fetchall()
+        Map_transaction=pd.DataFrame(map_trans,columns=["Districts", "Transaction_amount"])
+        mt= Map_transaction[["Districts", "Transaction_amount"]]
+        mt1= mt.groupby("Districts")["Transaction_amount"].sum().sort_values(ascending=False)
+        mt2= pd.DataFrame(mt1).head(10).reset_index()
+
+        fig_mt= px.line(mt2, x= "Transaction_amount", y= "Districts", title="TOP 10 DISTRICTS OF HIGHEST TRANSACTION AMOUNT",
+                        color_discrete_sequence=px.colors.sequential.Greens_r)
+        st.plotly_chart(fig_mt)
+
+    if Querys=='Top 10 Districts With Lowest Transaction Amount':
+        cursor.execute("select District,Transaction_Amount from map_trans")
+        map_trans = cursor.fetchall()
+        Map_transaction=pd.DataFrame(map_trans,columns=["Districts", "Transaction_amount"])
+        lmt= Map_transaction[["Districts", "Transaction_amount"]]
+        lmt1= lmt.groupby("Districts")["Transaction_amount"].sum().sort_values(ascending=True)
+        lmt2= pd.DataFrame(lmt1).head(10).reset_index()
+
+        fig_lmt= px.scatter(lmt2, x= "Transaction_amount", y= "Districts", title="TOP 10 DISTRICTS OF LOWEST TRANSACTION AMOUNT",
+                            color_discrete_sequence=px.colors.sequential.Redor_r)
+        st.plotly_chart(fig_lmt)
+
+    if Querys=='Top states with highest Users':
+        cursor.execute("select State,Total_users from map_user")
+        map_user = cursor.fetchall()
+        map_user1=pd.DataFrame(map_user,columns=["State","Total_users"])
+        map_user2= map_user1[["State","Total_users"]]
+        map_user3= map_user2.groupby("State")["Total_users"].sum().sort_values(ascending=False)
+        map_user4= pd.DataFrame(map_user3).reset_index()
+
+        fig_brand= px.pie(map_user4, values= "Total_users", names= "State", color_discrete_sequence=px.colors.sequential.dense_r,
+                            title= "Top states with highest Users")
+        st.plotly_chart(fig_brand)
+    
+    if Querys=='States With Lowest Transaction Count':
+        cursor.execute("select State,Transaction_count from agg_trans")
+        Aggre_transaction = cursor.fetchall()
+        Aggre_transaction1=pd.DataFrame(Aggre_transaction,columns=["States","Transaction_count"])
+        stc= Aggre_transaction1[["States", "Transaction_count"]]
+        stc1= stc.groupby("States")["Transaction_count"].sum().sort_values(ascending=True)
+        stc2= pd.DataFrame(stc1).reset_index().head(10)
+
+        fig_stc= px.bar(stc2, x= "States", y= "Transaction_count", title= "STATES WITH LOWEST TRANSACTION COUNT",orientation='h',
+                            color_discrete_sequence= px.colors.sequential.Jet_r)
+        st.plotly_chart(fig_stc)
+
+    if Querys=='States With Highest Transaction Count':
+        cursor.execute("select State,Transaction_count from top_trans")
+        Aggre_transaction = cursor.fetchall()
+        Aggre_transaction1=pd.DataFrame(Aggre_transaction,columns=["States","Transaction_count"])
+        stc= Aggre_transaction1[["States", "Transaction_count"]]
+        stc1= stc.groupby("States")["Transaction_count"].sum().sort_values(ascending=False)
+        stc2= pd.DataFrame(stc1).reset_index().head(10)
+
+        fig_stc= px.bar(stc2, x= "States", y= "Transaction_count", title= "STATES WITH HIGHEST TRANSACTION COUNT",
+                            color_discrete_sequence= px.colors.sequential.Jet_r)
+        st.plotly_chart(fig_stc)
+
+    if Querys=='States With Highest Transaction Amount':
+        cursor.execute("select State,Transaction_amount from top_trans")
+        Aggre_transaction = cursor.fetchall()
+        Aggre_transaction1=pd.DataFrame(Aggre_transaction,columns=["States","Transaction_amount"])
+        stc= Aggre_transaction1[["States", "Transaction_amount"]]
+        stc1= stc.groupby("States")["Transaction_amount"].sum().sort_values(ascending=False)
+        stc2= pd.DataFrame(stc1).reset_index().head(10)
+
+        fig_stc= px.line(stc2, x= "States", y= "Transaction_amount", title= "States with Highest Transaction_amount",
+                            color_discrete_sequence= px.colors.sequential.Jet_r)
+        st.plotly_chart(fig_stc)
+
+    if Querys=='Top 50 Districts With Lowest Transaction Amount':
+        cursor.execute("select District,Transaction_Amount from map_trans")
+        map_transaction = cursor.fetchall()
+        map_transaction1=pd.DataFrame(map_transaction,columns=["Districts","Transaction_amount"])
+        dt= map_transaction1[["Districts", "Transaction_amount"]]
+        dt1= dt.groupby("Districts")["Transaction_amount"].sum().sort_values(ascending=True)
+        dt2= pd.DataFrame(dt1).reset_index().head(50)
+
+        fig_dt= px.bar(dt2, x= "Districts", y= "Transaction_amount", title= "DISTRICTS WITH LOWEST TRANSACTION AMOUNT",
+                        color_discrete_sequence= px.colors.sequential.Mint_r)
+        st.plotly_chart(fig_dt)
+    
+
+    
+
 
